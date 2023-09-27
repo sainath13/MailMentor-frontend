3ae880f99cba5fe9ea7b2fbc04186da81087579b
sainathl <latkarsainath@gmail.com>
2023-09-27 21:42:48 +0530

    [init] with working ai response

---
+++ src/components/icons/IconAI.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const IconAI = () => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="8" cy="8" r="7" stroke="current" strokeWidth="2" fill="none" />
+      <path
+        d="M8 2V8M8 8L5.5 10.5M8 8L10.5 10.5"
+        stroke="current"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
+
+export default IconAI;
